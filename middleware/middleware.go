f4d00e5552bf709e5a850710567bb7305d66b5b3
aleqxan <inno.alex@ymail.com>
2022-05-13 04:21:32 +0200

    Updated configurations for Token, middleware and cart

---
+++ middleware/middleware.go
@@ -0,0 +1,29 @@
+package middleware
+
+import (
+	"net/http"
+
+	token "github.com/Aleqxan/E-commerce-project/tokens"
+	"github.com/gin-gonic/gin"
+)
+
+func Authentication() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ClientToken := c.Request.Header.Get("token")
+		if ClientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "No authorixation header provided"})
+			c.Abort()
+			return
+		}
+		cliams, err := token.ValidateToken(ClientToken)
+		if err != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+
+		c.Set("email", claims.Email)
+		c.Set("uid", claims.Uid)
+		c.Next()
+	}
+}
